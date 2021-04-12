d8984d2548a0c920eb8e7a74f4d92767d0f9d875
mits <dmitrybaycharov@gmail.com>
2021-04-12 17:54:42 -0500

    Step Four: Posting a Score finished

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,27 @@
+let score = 0;
+let guesses = []
 
 $('#submit-btn').on('click', handleGuess);
 
 async function handleGuess(e) {
     e.preventDefault();
+
     let guess = $('#word').val();
     let res = await axios.get('/guess', { params: { guess: guess } });
-    response = res.data.response
-    $('#response').text(response); //set innertext of 
+    response = res.data.response;
+    $('#response').text(response); //set innertext  
+
+    //do not increment score if already guessed
+    let alreadyGuessed = guesses.find(e => e == guess);
+    guesses.push(guess)
+    if(alreadyGuessed) {
+        alert('Already Guessed'); 
+    }
+    //if 'ok' then score++ and rerender score
+    else if(response == 'ok') {
+        score += guess.length;
+        $('#score').text(score);
+    }
+
 }
 
