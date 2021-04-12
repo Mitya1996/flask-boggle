0b63faff3dcb836198cc4e3a592d3651be0d979a
mits <dmitrybaycharov@gmail.com>
2021-04-12 11:48:27 -0500

    add venv, flask, and debugtoolbar

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from flask import Flask
 from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = 'trolol'
+
+toolbar = DebugToolbarExtension(app)
+
 
 boggle_game = Boggle()
