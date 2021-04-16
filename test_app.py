0971799af7013f08d3f1a6272e065cc73b035759
mits <dmitrybaycharov@gmail.com>
2021-04-15 21:03:53 -0500

    flask testing context question

--- test_app.py
+++ test_app.py
@@ -7,6 +7,19 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    @classmethod
+    def setUpClass(cls):
+        with app.test_client() as client:
+
+            test_game = Boggle()
+            test_board = test_game.make_board()
+            with client.session_transaction() as change_session:
+                change_session['board'] = test_board
+
+    def setUp(self):
+        app.config['TESTING'] = True
+
+
     def test_home(self):
         with app.test_client() as client:
             resp = client.get('/')
@@ -16,13 +29,19 @@ class FlaskTests(TestCase):
             #check if Boggle h1 is in the response
             self.assertIn('<h1>Boggle</h1>', html)
 
-    def test_guess(self):
+    def test_guess_not_word(self):
         with app.test_client() as client:
-            test_game = Boggle()
-            board = test_game.make_board()
 
-            with client.session_transaction() as change_session:
-                change_session['board'] = board
+            # test_game = Boggle()
+            # test_board = test_game.make_board()
+            # with client.session_transaction() as change_session:
+            #     change_session['board'] = test_board
+
             resp = client.get('/guess?guess=asdf') #test a 'not-a-word'
-            message = resp.get_data()
-            self.assertIn("not-word", message)
+            message = resp.json['response']
+
+            self.assertEqual("not-word", message)
+            self.assertEqual(resp.status_code, 200)
+
+    # def test_guess_not_on_board(self):
+    #     with app.test_client() as client:
