78ca078bd26202c8279945ed4b222b07b859aa14
mits <dmitrybaycharov@gmail.com>
2021-04-16 16:31:21 -0500

    testing refactoring

--- test_app.py
+++ test_app.py
@@ -12,57 +12,51 @@ class FlaskTests(TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
+        self.client = app.test_client()
+        with self.client as client:
 
+            test_game = Boggle()
+            test_board = test_game.make_board()
+            with client.session_transaction() as change_session:
+                change_session['board'] = test_board
 
     def test_home(self):
-        with app.test_client() as client:
-            resp = client.get('/')
-            html = resp.get_data(as_text=True)
-            #check if status code 200
-            self.assertEqual(resp.status_code, 200)
-            #check if Boggle h1 is in the response
-            self.assertIn('<h1>Boggle</h1>', html)
+        resp = self.client.get('/')
+        html = resp.get_data(as_text=True)
+        #check if status code 200
+        self.assertEqual(resp.status_code, 200)
+        #check if Boggle h1 is in the response
+        self.assertIn('<h1>Boggle</h1>', html)
 
     def test_guess_ok(self):
-        with app.test_client() as client:
-            test_board = [["C", "A", "T", "T", "T"], 
-                          ["C", "A", "T", "T", "T"], 
-                          ["C", "A", "T", "T", "T"], 
-                          ["C", "A", "T", "T", "T"], 
-                          ["C", "A", "T", "T", "T"]]
-            with client.session_transaction() as change_session:
-                change_session['board'] = test_board
-            
-            resp = client.get('/guess?guess=cat') 
-            message = resp.json['response']
+        test_board =   [["C", "A", "T", "T", "T"], 
+                        ["C", "A", "T", "T", "T"], 
+                        ["C", "A", "T", "T", "T"], 
+                        ["C", "A", "T", "T", "T"], 
+                        ["C", "A", "T", "T", "T"]]
+                        
+        with self.client.session_transaction() as change_session:
+            change_session['board'] = test_board
+        
+        resp = self.client.get('/guess?guess=cat') 
+        message = resp.json['response']
 
-            self.assertEqual("ok", message)
-            self.assertEqual(resp.status_code, 200)
+        self.assertEqual("ok", message)
+        self.assertEqual(resp.status_code, 200)
 
     def test_guess_not_word(self):
-        with app.test_client() as client:
 
-            test_game = Boggle()
-            test_board = test_game.make_board()
-            with client.session_transaction() as change_session:
-                change_session['board'] = test_board
+        resp = self.client.get('/guess?guess=asdf') #test a 'not-a-word'
+        message = resp.json['response']
 
-            resp = client.get('/guess?guess=asdf') #test a 'not-a-word'
-            message = resp.json['response']
-
-            self.assertEqual("not-word", message)
-            self.assertEqual(resp.status_code, 200)
+        self.assertEqual("not-word", message)
+        self.assertEqual(resp.status_code, 200)
 
     def test_guess_not_on_board(self):
-        with app.test_client() as client:
-            test_game = Boggle()
-            test_board = test_game.make_board()
-            with client.session_transaction() as change_session:
-                change_session['board'] = test_board
-            
-            resp = client.get('/guess?guess=helicopter') 
-            message = resp.json['response']
 
-            self.assertEqual("not-on-board", message)
-            self.assertEqual(resp.status_code, 200)
+        resp = self.client.get('/guess?guess=helicopter') 
+        message = resp.json['response']
+
+        self.assertEqual("not-on-board", message)
+        self.assertEqual(resp.status_code, 200)
 
